60cf1585fff4fdf52401bb37b1cae975fadef103
Saikat Majumder <saikat.majumder@buzzybrains.com>
2022-03-26 13:42:59 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder,FormGroup, Validators} from "@angular/forms"
+import { compareValue } from 'src/shared/validators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'reactive-form';
+  signUpForm!:FormGroup;
+  submitted=false;
+  constructor(private fb:FormBuilder){}
+
+  ngOnInit(): void {
+    this.signUpForm=this.fb.group({
+      firstName:["",Validators.required],
+      lastName:["",Validators.required],
+      email:["",[Validators.required,Validators.email]],
+      passWord:["",[Validators.required,Validators.minLength(6)]],
+      cPassword:["",Validators.required],
+      termsAndConditions:[false,Validators.requiredTrue],
+
+    },
+    {
+      validator: compareValue("passWord","cPassword")
+    }
+    )
+  }
+
+  get h():any{
+    return this.signUpForm.controls
+  }
+
+  resetForm(){
+    this.submitted=false;
+    this.signUpForm.reset()
+  }
+
+  submitForm(){
+    this.submitted=true;
+    if(this.signUpForm.invalid){
+      return
+    }
+    else{
+      alert(JSON.stringify(this.signUpForm.value))
+    }
+  }
 }
---
+++ src/shared/validators.ts
@@ -0,0 +1,13 @@
+import { AbstractControl, FormGroup } from '@angular/forms';
+export function compareValue(controlName: string, matchingControlName: string) {
+    return (formGroup: FormGroup) => {
+        let control = formGroup.controls[controlName];
+        let matchingControl = formGroup.controls[matchingControlName]
+        if (control.value !== matchingControl.value) {
+            matchingControl.setErrors({ confirmPasswordValidator: true });
+        } else {
+            matchingControl.setErrors(null);
+        }
+    }
+
+}
